26564fa7bd486c7c1e6270dbe34ef3fb18ee22dd
Emerson <emersonevandro_@hotmail.com>
2022-07-22 22:29:26 -0300

    Cypress

---
+++ cypress/integration/testes/arrow.spec.js
@@ -0,0 +1,27 @@
+it('nada agora', function() {})
+
+/*function soma(a, b) {
+    return a + b;
+}*/
+
+/*const soma = function(a, b) {
+    return a + b;
+}*/
+
+/*const soma = (a, b) => {
+    return a + b;
+}*/
+
+//const soma = (a, b) => a + b;
+
+//const soma = (a) => a + a;
+
+//const soma = a => a + a;
+
+const soma = () => 5 + 5;
+
+it('a fuction test...', function() {
+    console.log('Function', this);
+})
+
+console.log(soma(1,4));
